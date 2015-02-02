39578799c224e9b29c3e94bfb0412b04df27b384
AnAnaatti <naattiana@gmail.com>
2015-02-02 20:14:16 +0200

    Lisätty kytkin ja virtalähdeskripti

---
+++ Assets/Kytkin.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kytkin : MonoBehaviour {
+
+	public bool isalive;
+	public int sources;
+	public float hp;
+	public bool isturnedon;
+
+	public GameObject[] connectedobjects;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		turnedoncheck ();
+		if (sources == 0) {   //testailua siitä onko sähköt päällä
+						gameObject.renderer.material.color = Color.red;
+				} else if (sources == 1) {
+						gameObject.renderer.material.color = Color.green;
+				} else {
+			gameObject.renderer.material.color = Color.yellow;
+				}
+	
+	}
+
+	public void setpowers(int i) //asettaa powereita
+	{
+
+		sources = sources + i;
+	}
+
+	public void alivecheck()
+	{
+
+	}
+
+	public void turnedoncheck()
+	{
+		if ((isturnedon && sources < 1) || (!isturnedon && sources > 0))
+		    {
+			isturnedon = !isturnedon;
+			if (isturnedon) //meni päälle
+			{
+				for (int i = 0; i < connectedobjects.Length; i++)
+				{
+					connectedobjects[i].SendMessage("setpowers", 1); //lähetä viesti lisäämään powereja
+					
+				}
+			}
+			else
+			{
+				for (int i = 0; i < connectedobjects.Length; i++)
+				{
+					connectedobjects[i].SendMessage("setpowers", -1); //lähetä viesti vähentää powereja
+					
+				}
+			}
+
+
+		}
+	}
+
+
+
+
+}
---
+++ Assets/PowerSourceScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class PowerSourceScript : MonoBehaviour {
+
+	public GameObject[] connectedpowers;
+	public float hp;
+	public bool isalive;
+	public bool isturnedon;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		alivecheck ();
+		if (isalive) {
+
+				}
+	
+	}
+
+	void alivecheck()
+	{
+		if (hp <= 0 && isalive)
+		{
+			isalive = false;
+			if (isturnedon)
+			{
+			for (int i = 1; i < connectedpowers.Length; i++) //ilmoita kaikille kytkimille kun kuolen :(
+			{
+				Kytkin kytkin = connectedpowers[i].GetComponent<Kytkin>();
+				kytkin.setpowers(-1);
+
+			}
+			}
+		}
+	}
+
+	void OnMouseOver(){
+		if(Input.GetMouseButtonDown(0)){
+			isturnedon = !isturnedon;
+			if (isturnedon) // jos poweri käynnistyy ilmoita kaikille kytkimille
+			{
+				for (int i = 0; i < connectedpowers.Length; i++)
+				{
+
+					Kytkin kytkin = connectedpowers[i].GetComponent<Kytkin>();
+					kytkin.setpowers(1);
+					
+				}
+
+			}
+			if (!isturnedon) // jos poweri menee pois päälle ilmoita kaikille kytkimille
+			{
+				for (int i = 0; i < connectedpowers.Length; i++)
+				{
+					Kytkin kytkin = connectedpowers[i].GetComponent<Kytkin>();
+					kytkin.setpowers(-1);
+					
+				}
+				
+			}
+
+		}
+
+	}
+}
